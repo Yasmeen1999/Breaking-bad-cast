973ccf1c660bf6f428846ec1f1ede0e5843317fc
yasmeen1999 <yasmeenaleemkhan786@gmail.com>
2021-02-13 15:28:44 +0530

    the breaking bad cast api intigration

---
+++ src/componets/Header.js
@@ -0,0 +1,13 @@
+import style from "./header.module.css";
+
+const Header = () => {
+    return (  
+        <div className={style.header} >
+            
+            <h1>The Breaking Bad characters</h1>
+           
+        </div>
+    );
+}
+ 
+export default Header;
